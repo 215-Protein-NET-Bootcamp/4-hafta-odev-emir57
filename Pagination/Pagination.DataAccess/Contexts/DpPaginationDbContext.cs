44ed48c910af19a94633ce3eaede2c20a26624ec
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 15:04:23 +0300

    EfPaginationDbContext and Create IContext Interface

--- Pagination/Pagination.DataAccess/Contexts/DpPaginationDbContext.cs
+++ Pagination/Pagination.DataAccess/Contexts/DpPaginationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Core.Utilities.Helpers;
+﻿using Core.DataAccess;
+using Core.Utilities.Helpers;
 using Npgsql;
 using System.Data;
 
 namespace Pagination.DataAccess.Contexts
 {
-    public class DpPaginationDbContext
+    public class DpPaginationDbContext : IContext
     {
         public IDbConnection CreateConnection()
         {
