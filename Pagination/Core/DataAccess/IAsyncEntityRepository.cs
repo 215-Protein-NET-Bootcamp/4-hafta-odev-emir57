b61be1da286b38f2e87e77333183c3369e55ce4d
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 14:37:54 +0300

    Create IAsyncEntityRepository

---
+++ Pagination/Core/DataAccess/IAsyncEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IAsyncEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> GetListAsync();
+        Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
