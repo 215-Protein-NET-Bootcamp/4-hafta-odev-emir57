9946f0a168b6037980eaa2b7dd7efa74596d0789
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 14:21:40 +0300

    PersonDto

---
+++ Pagination/Pagination.Dto/Concrete/PersonDto.cs
@@ -0,0 +1,14 @@
+﻿using Core.Dto;
+
+namespace Pagination.Dto.Concrete
+{
+    public class PersonDto : IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string? Description { get; set; }
+        public string? Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
