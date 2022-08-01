12c885cee4b043f1400850bf6e0650ac8e580c0f
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 14:17:34 +0300

    Person Entity

---
+++ Pagination/Pagination.Entity/Concrete/Person.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entity;
+
+namespace Pagination.Entity.Concrete
+{
+    public class Person : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string? Description { get; set; }
+        public string? Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
