e652023fa7d9ad808222c669f607c74f3a14df12
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 14:29:02 +0300

    Create Business Module and Register in Program.cs

---
+++ Pagination/Pagination.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Pagination.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
--- Pagination/Pagination.WebApi/Program.cs
+++ Pagination/Pagination.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Pagination.Business.DependencyResolvers.Autofac;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Register AutofacBusinessModule
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
+#endregion
 
 var app = builder.Build();
 
