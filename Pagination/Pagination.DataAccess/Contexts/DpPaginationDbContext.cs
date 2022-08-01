d82cdd686c4af5ae0d445f8b21f72ebf802adb5a
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 14:58:49 +0300

    Create ConnectionStringHelper

---
+++ Pagination/Pagination.DataAccess/Contexts/DpPaginationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagination.DataAccess.Contexts
+{
+    public class DpPaginationDbContext
+    {
+        public IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection();
+        }
+    }
+}
