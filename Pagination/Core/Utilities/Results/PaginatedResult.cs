05ea433515de0a0a43bbc371ce3fe9f3a9271934
emir57 <emir.gurbuz06@hotmail.com>
2022-08-02 15:21:03 +0300

    Create PaginatedResult

---
+++ Pagination/Core/Utilities/Results/PaginatedResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class PaginatedResult<T> : IDataResult<T>
+    {
+        public PaginatedResult(T data, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber == 0 ? 1 : pageNumber;
+            PageSize = pageSize == 0 ? 10 : pageSize;
+
+            Success = true;
+            Data = data;
+            Message = "Success pagination list";
+        }
+        public T Data { get; set; }
+        public bool Success { get; }
+        public string Message { get; set; }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public System.Uri FirstPage { get; set; }
+        public System.Uri LastPage { get; set; }
+        public System.Uri NextPage { get; set; }
+        public System.Uri PreviousPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+
+    }
+}
