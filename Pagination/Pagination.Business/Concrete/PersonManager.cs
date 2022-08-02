2e3024ad57ff5c96ef2b29aeb64683f105a2fde3
emir57 <emir.gurbuz06@hotmail.com>
2022-08-02 14:15:06 +0300

    PersonManager constructor

--- Pagination/Pagination.Business/Concrete/PersonManager.cs
+++ Pagination/Pagination.Business/Concrete/PersonManager.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
-using Core.DataAccess;
-using Core.Utilities.Results;
 using Pagination.Business.Abstract;
+using Pagination.DataAccess.Abstract;
 using Pagination.Dto.Concrete;
 using Pagination.Entity.Concrete;
 
@@ -9,7 +8,7 @@ namespace Pagination.Business.Concrete
 {
     public class PersonManager : AsyncBaseManager<Person, PersonDto>, IPersonService
     {
-        public PersonManager(IAsyncEntityRepository<Person> repository, IMapper mapper) : base(repository, mapper)
+        public PersonManager(IPersonDal repository, IMapper mapper) : base(repository, mapper)
         {
         }
     }
