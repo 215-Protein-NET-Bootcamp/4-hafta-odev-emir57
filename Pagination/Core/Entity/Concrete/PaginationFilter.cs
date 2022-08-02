15610898a29863195a11a96b1de38e13aa7f23e6
emir57 <emir.gurbuz06@hotmail.com>
2022-08-02 15:32:05 +0300

    PaginationFilter

---
+++ Pagination/Core/Entity/Concrete/PaginationFilter.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Entity.Concrete
+{
+    public class PaginationFilter
+    {
+        public PaginationFilter()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            PageSize = pageSize < 1 ? 10 : pageSize;
+        }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ Pagination/Pagination.Business/Helpers/PaginationHelper.cs
@@ -0,0 +1,9 @@
+﻿using Core.Utilities.Results;
+
+namespace Pagination.Business.Helpers
+{
+    public static class PaginationHelper
+    {
+        public static PaginatedResult CreatePaginatedResponse<T>(IEnumerable<T> data,Paginat)
+    }
+}
