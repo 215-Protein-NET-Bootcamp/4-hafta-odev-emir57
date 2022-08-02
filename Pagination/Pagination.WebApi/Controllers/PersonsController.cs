b5bac12dd3f6081ffad330358f87d0198e10f35e
emir57 <emir.gurbuz06@hotmail.com>
2022-08-02 14:25:00 +0300

    Create PersonsController

---
+++ Pagination/Pagination.WebApi/Controllers/PersonsController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Pagination.Business.Abstract;
+using Pagination.Dto.Concrete;
+using Pagination.Entity.Concrete;
+
+namespace Pagination.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonsController : BaseController<Person, PersonDto>
+    {
+        public PersonsController(IPersonService service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
