27c97f32ff2a3ef3c0610fb419162ff220f67797
emir57 <emir.gurbuz06@hotmail.com>
2022-08-02 14:08:48 +0300

    Create IPersonService and PersonManager

--- Pagination/Pagination.Business/Abstract/IPersonService.cs
+++ Pagination/Pagination.Business/Abstract/IPersonService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Pagination.Dto.Concrete;
+using Pagination.Entity.Concrete;
 
 namespace Pagination.Business.Abstract
 {
-    internal class IPersonService
+    public interface IPersonService : IAsyncBaseService<Person, PersonDto>
     {
     }
 }
---
+++ Pagination/Pagination.Business/Concrete/PersonManager.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Core.DataAccess;
+using Core.Utilities.Results;
+using Pagination.Business.Abstract;
+using Pagination.Dto.Concrete;
+using Pagination.Entity.Concrete;
+
+namespace Pagination.Business.Concrete
+{
+    public class PersonManager : AsyncBaseManager<Person, PersonDto>, IPersonService
+    {
+        public PersonManager(IAsyncEntityRepository<Person> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
