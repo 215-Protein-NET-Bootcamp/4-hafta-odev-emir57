d363f72eceaf46ccc67dd65d69b267ea930c24c0
emir57 <emir.gurbuz06@hotmail.com>
2022-08-05 01:14:19 +0300

    Complete Pagination_return_10_rows Test

--- Pagination/Pagination.Business.Test/PersonManagerTest.cs
+++ Pagination/Pagination.Business.Test/PersonManagerTest.cs
@@ -33,7 +33,7 @@ namespace Pagination.Business.Test
         [InlineData(10, 1)]
         public async Task Pagination_return_10_rows(int pageSize, int pageNumber)
         {
-            _personDalMock.Setup(x => x.GetListAsync(pageSize, (pageNumber == 1 ? 0 : pageNumber) * pageSize)).ReturnsAsync(getPersons());
+            _personDalMock.Setup(x => x.GetListAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync((int limit, int offset) => getPersons(pageSize, pageNumber));
             _cacheManager.Setup(x => x.IsAdd("patika")).Returns(false);
 
             PersonManager personManager = new PersonManager(_personDalMock.Object, _mapper, _cacheManager.Object);
@@ -49,12 +49,13 @@ namespace Pagination.Business.Test
             Assert.Equal(personsResult.Data.Count(), 10);
         }
 
-        private IEnumerable<Person> getPersons()
+        private IEnumerable<Person> getPersons(int pageSize, int pageNumber)
         {
+            List<Person> returnPersons = new List<Person>();
             var random = new Random();
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                yield return new Person
+                returnPersons.Add(new Person
                 {
                     Id = i + 1,
                     FirstName = $"Emir{i}",
@@ -62,9 +63,13 @@ namespace Pagination.Business.Test
                     Email = $"emir{i}@hotmail.com",
                     Description = "lorem",
                     Phone = "000",
-                    DateOfBirth = new DateTime(random.Next(1980, DateTime.Now.Year), random.Next(1, 12), random.Next(1, 30))
-                };
+                    DateOfBirth = new DateTime(random.Next(1980, DateTime.Now.Year), random.Next(1, 12), random.Next(1, 29))
+                });
             }
+            return returnPersons
+                .Skip((pageNumber == 1 ? 0 : pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .AsEnumerable();
         }
     }
 }
